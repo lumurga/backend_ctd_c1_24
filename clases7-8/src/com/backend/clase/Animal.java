15ab6fb34976c1c30e115d3988c24eeb304c7997
lumurga <mlucianamurga@gmail.com>
2024-02-15 22:12:56 -0300

    Clases 7 y 8 - Log4j, Acceso a base de datos

---
+++ clases7-8/src/com/backend/clase/Animal.java
@@ -0,0 +1,71 @@
+package com.backend.clase;
+
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+
+public class Animal {
+    public static void main(String[] args) {
+
+        Logger LOGGER = Logger.getLogger(Animal.class);
+
+        String create = "DROP TABLE IF EXISTS ANIMALES; CREATE TABLE ANIMALES(ID INT AUTO_INCREMENT PRIMARY KEY, NOMBRE VARCHAR(50) NOT NULL, TIPO VARCHAR(50) NOT NULL)";
+        String insert = "INSERT INTO ANIMALES(NOMBRE, TIPO) VALUES ('Firulais', 'Perro'), ('Lola', 'Vaca'), ('Homero', 'Perro'), ('Pepe', 'Sapo'), ('Tuki', 'Loro')";
+
+        Connection connection = null;
+        try{
+            connection = getConnection();
+
+            //crear tabla
+            Statement statement = connection.createStatement();
+            statement.execute(create);
+
+            //insertar registros en la tabla
+            statement.execute(insert);
+
+            //select all
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ANIMALES");
+
+
+            //recorrer el result set
+            while(resultSet.next()){
+                LOGGER.info("Animal: " + resultSet.getInt(1) + " - " + resultSet.getString("nombre") + " - " + resultSet.getString("tipo"));
+            }
+
+            //eliminar un registro
+            statement.execute("DELETE FROM ANIMALES WHERE ID = 1");
+            LOGGER.info("----------------------------------------------------------------");
+
+            //select all
+            resultSet = statement.executeQuery("SELECT * FROM ANIMALES");
+
+            //recorrer el result set
+            while(resultSet.next()){
+                LOGGER.info("Animal: " + resultSet.getInt(1) + " - " + resultSet.getString("nombre") + " - " + resultSet.getString("tipo"));
+            }
+
+
+        } catch (Exception exception){
+            exception.printStackTrace();
+            LOGGER.error(exception.getMessage());
+        }
+
+        finally {
+            try{
+                connection.close();
+            } catch (Exception exception){
+                LOGGER.error(exception.getMessage());
+            }
+        }
+
+    }
+
+    //metodo para poder establecer una conexion cuando la necesite
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        //indicar que driver vamos a usar
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:~/clase8c1", "sa", "sa");
+        //jdbc:h2:mem:testdb en memoria
+    }
+
+}
